af752a7135a05d91fac4bbd52b11bb27791eadbb
IDeltz <danilloaugusto5682gmail.com>
2025-06-02 20:31:50 -0300

    Atividade 6 – Funcionários com abstração

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-from produto import Produto
+from produto import FuncionarioCLT, FuncionarioPJ, Produto, ProdutoImportado, ProdutoNacional
 
 
 # Exemplo de uso:
 
-p = Produto("Monitor", 800.00, 10)
+# Testando Produto e subclasses
+print("\n--- Produtos ---")
+p1 = Produto("Teclado", 100.0, 10)
+p2 = ProdutoNacional("Monitor", 800.0, 5)
+p3 = ProdutoImportado("Celular", 2000.0, 2, 0.2)
 
-p.exibir_detalhes()
-p.vender(3)
-p.repor(5)
-p.vender(20)   # Estoque insuficiente
-p.vender(-1)   # Valor inválido
-p.repor(0)     # Valor inválido
+p1.exibir_detalhes()
+p2.exibir_detalhes()
+p3.exibir_detalhes()
+
+print(f"Preço final do {p3.nome}: R${p3.preco_final():.2f}")
+p3.emitir_nota()
+
+# Testando Funcionários
+print("\n--- Funcionários ---")
+f1 = FuncionarioCLT("João", 3500.00)
+f2 = FuncionarioPJ("Mariana", 160, 50)
+
+print(f"{f1.nome} vai receber R${f1.calcular_pagamento():.2f}")
+print(f"{f2.nome} vai receber R${f2.calcular_pagamento():.2f}")
